fbc9a58f3c6eea3ca908e504cc52b03050074eec
bifacial2 <fedets.stanislav@gmail.com>
2022-04-28 14:16:40 +0000

    Deploying to gh-pages from @ bifacial2/starbucks@9d31d175b3b285011f2ff35002e50224407993b8 🚀

--- src.961a4696.js
+++ src.961a4696.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.22508b25.js.map
+//# sourceMappingURL=/parcel-project-template/src.961a4696.js.map
